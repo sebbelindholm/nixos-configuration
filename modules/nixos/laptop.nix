f61c0d5e2bc8bea3bab9727cf257adccd3fe6d0d
Sebastian Lindholm Gustafsson <sebastianlindholm@outlook.com>
2024-09-04 18:46:35 +0200

    Update.

--- modules/nixos/laptop.nix
+++ modules/nixos/laptop.nix
@@ -11,7 +11,7 @@ in
 
   config = lib.mkIf cfg.enable {
         services.tlp = {
-            enable = true;
+            enable = false;
                 settings = {
                     CPU_SCALING_GOVERNOR_ON_AC = "performance";
                     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
