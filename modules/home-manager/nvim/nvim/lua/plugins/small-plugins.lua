ad7177d124825c9f4ed442283d901ae63106b2ad
Sebastian Lindholm Gustafsson <sebastianlindholm@outlook.com>
2024-09-09 11:50:48 +0200

    Update.

--- modules/home-manager/nvim/nvim/lua/plugins/small-plugins.lua
+++ modules/home-manager/nvim/nvim/lua/plugins/small-plugins.lua
@@ -1,23 +1,26 @@
-require("nvim-autopairs").setup({ disable_filetype = { "TelescopePrompt" , "vim", "neo-tree" } })
+require("nvim-autopairs").setup({ disable_filetype = { "TelescopePrompt", "vim", "neo-tree" } })
 require("dashboard").setup({ theme = "hyper" })
 require("Comment").setup()
-require("dressing").setup({ enabled = true, })
+require("dressing").setup({ enabled = true })
 require("gitsigns").setup({})
 require("which-key").setup({})
-require('colorizer').setup({})
+require("colorizer").setup({})
 require("fidget").setup({})
 require("numb").setup({})
 require("nvim-surround").setup({})
 require("ibl").setup({
-    indent = { char = "▏" },
-    scope = {
-        enabled = true,
-        show_start = false,
-        include = {
-            node_type = {
-                lua = { "return_statement", "table_constructor" },
-                ["*"] = { "object", "arguments", "named_imports", "class_body" },
-            },
-        },
-    },
+	indent = { char = "▏" },
+	scope = {
+		enabled = true,
+		show_start = false,
+		include = {
+			node_type = {
+				lua = { "return_statement", "table_constructor" },
+				["*"] = { "object", "arguments", "named_imports", "class_body" },
+			},
+		},
+	},
+	exclude = {
+		filetypes = { "dashboard" },
+	},
 })
