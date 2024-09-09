5f5c1957857f55e650275f0fc5812bff45090807
Sebastian Lindholm Gustafsson <sebastianlindholm@outlook.com>
2024-09-09 14:11:16 +0200

    Update.

--- modules/home-manager/nvim/nvim/lua/plugins/completions.lua
+++ modules/home-manager/nvim/nvim/lua/plugins/completions.lua
@@ -44,9 +44,9 @@ cmp.setup.cmdline({ "/", "?" }, {
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(":", {
-	mapping = cmp.mapping.preset.cmdline(),
+	---mapping = cmp.mapping.preset.cmdline(),
 	sources = cmp.config.sources({
-		{ name = "path" },
+		---{ name = "path" },
 	}, {
 		{ name = "cmdline" },
 	}),
