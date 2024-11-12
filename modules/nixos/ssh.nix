a636fa57e01dfc8a5e098d12d202b6694bf6249f
Sebastian Lindholm Gustafsson <sebastianlindholm@outlook.com>
2024-11-12 23:20:36 +0100

    Update.

--- modules/nixos/ssh.nix
+++ modules/nixos/ssh.nix
@@ -21,8 +21,9 @@ in
         KbdInteractiveAuthentication = false;
       };
     };
-    users.users.sebastian.openssh.authorizedKeys.keys =
-      [
-      ];
+    users.users.sebastian.openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIO57I6zrUaKV7/WvVhdNjO9Dyabd0BDyp1n4hWXg9cw sebastian@nixos"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINPJwWddmvo2Ogi4LT6fQ8NHo0JuAJj59URC8OJTrUWR sebastian@nixos-desktop"
+    ];
   };
 }
