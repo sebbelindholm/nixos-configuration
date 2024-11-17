72d22cb9495b6b37e2be64892938d5b6d1c3ff18
Sebastian Lindholm Gustafsson <sebastianlindholm@outlook.com>
2024-11-17 02:25:23 +0100

    Update.

--- modules/home-manager/hypr/hyprland.nix
+++ modules/home-manager/hypr/hyprland.nix
@@ -99,13 +99,14 @@
           xray = true;
         };
 
-        drop_shadow = true;
-
-        shadow_ignore_window = true;
-        shadow_offset = "0 2";
-        shadow_range = 20;
-        shadow_render_power = 3;
-        "col.shadow" = "rgba(00000055)";
+        shadow = {
+          enabled = true;
+          ignore_window = false;
+          offset = "0 2";
+          range = 20;
+          render_power = 3;
+          color = "rgba(00000055)";
+        };
       };
 
       animations = {
