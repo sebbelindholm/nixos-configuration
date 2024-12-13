0ee739ad84f15ded1328104d0adc5be2259dfc20
Sebastian Lindholm Gustafsson <sebastianlindholm@outlook.com>
2024-12-14 00:59:38 +0100

    Update.

--- modules/nixos/xserver-module.nix
+++ modules/nixos/xserver-module.nix
@@ -8,7 +8,8 @@
 
 {
   services.xserver.enable = true;
-  services.xserver.desktopManager.budgie.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
   xdg.portal.enable = true;
 
   programs.hyprland = {
