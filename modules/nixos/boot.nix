ab7ac022c9e2d7a1004720b0efd79145f24d8f85
Sebastian Lindholm Gustafsson <sebastianlindholm@outlook.com>
2025-01-16 13:30:00 +0100

    Update.

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -7,5 +7,6 @@
 {
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.loader.systemd-boot.enable = true;
+  boot.initrd.systemd.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 }
